ccf614ad3cc273309e352e601d4eb20e530f4d76
nikolalkc <nikolalkc@hotmail.com>
2019-04-18 10:27:08 +0200

    Create LKC - Select next track for importing.lua

---
+++ Misc/LKC - Select next track for importing.lua
@@ -0,0 +1,26 @@
+--[[
+  ReaScript Name: Select Next Track For Importing
+  Author: LKC
+  REAPER: 5.0 pre 40
+  Version: 1.2
+  About:
+    Assign this script to a key in MediaExporer Section of ActionList. After running this script. Edit cursor goes back to start of selected item.
+    After that next track is selected. This allows you to double click on item in media exploerer and it will be imported one track under last selected item,
+    rather than selected item's track.
+    NOTE: Depends on Script: nikolalkc - Move Edit Cursor To Item Start.lua, change your ID if it doesn't work.
+]]
+
+--[[
+ * Changelog:
+	* v1.2 (2018-06-22)
+		+ Rename and new meta
+	* v1.1 (2017-06-13)
+		+ Support for horizontal zoom center to edit cursor
+	* v1.0 (2017-05-31)
+		+ Initial Release
+]]
+
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS4dca992a1547d86961758a325c95c73b2863280f"),0) --Script:Script: LKC - Move edit cursor to item start.lua
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_SELTRKWITEM"),0) --SWS: Select only track(s) with selected item(s)
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_SELNEXTTRACK"),0) --Xenakios/SWS: Select next tracks
+reaper.Main_OnCommand(40289,0) --Track: Unselect all items
